cf30ba0dd7305ba50192136500b2ecf46ff61c34
Jpbarrondev <161149890+Jpbarrondev@users.noreply.github.com>
2025-12-04 01:57:00 +0000

    Updated project to fix issues serving properly in GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Guides/',
+  base: '/react_hawt/',
   plugins: [react()],
   server: {
     port: 3000,
